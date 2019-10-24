721a4ef25a77e9b6df030ca33ce6b9c5959a84b7
Emmanuel Pius-Ogiji <emmanuel.pius-ogiji@c02xcbw4jg5j.gmia.banksvcs.net>
2019-10-24 13:29:19 +0100

    first commit for users

---
+++ test/unit_tests/test_calculator.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+from assertpy import assert_that
+
+
+class TestOperation(TestCase):
+    pass
+
+
+class TestHandler(TestCase):
+    pass
+
+
+class TestCalculator(TestCase):
+    pass
+
